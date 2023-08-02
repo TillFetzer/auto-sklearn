e4196cbefb58245f7e38fc5d8694707d267088e8
Till Fetzer <till.fetzer@googlemail.com>
2023-08-02 10:33:12 +0200

    runtime to less

--- examples/fairness/run_experiment.py
+++ examples/fairness/run_experiment.py
@@ -112,7 +112,7 @@ def run_experiment(datasets =["adult"],
                     if method == "moo+sar*ps":
                         base_sar_ps_com.run_experiment(dataset, constrain, sf[i], runtime, file, seed, runcount, under_folder, performance)   
                     if method == "lfr_test":
-                        lfr.run_experiment(dataset, constrain, sf[i], 5, file, seed, runcount, under_folder, performance)
+                        lfr.run_experiment(dataset, constrain, sf[i], runtime, file, seed, 5, under_folder, performance)
 
             print("all runs of {} finished".format(dataset))
     print("finished")
